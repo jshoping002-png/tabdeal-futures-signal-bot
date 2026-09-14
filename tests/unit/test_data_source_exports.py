5166aa700f5a5718d58544e273d5b11b74fced51
jshoping002-png <jshoping002@gmail.com>
2026-09-14 14:00:53 -0500

    test: cover data source package exports

---
+++ tests/unit/test_data_source_exports.py
@@ -0,0 +1,37 @@
+from tabdeal_signal.data_sources import (
+    DataProvenance,
+    DataQualityStatus,
+    DataSnapshotMetadata,
+    NormalizedSnapshot,
+    ReadOnlyDataSource,
+    SourceKind,
+)
+from tabdeal_signal.data_sources import __all__ as data_source_exports
+from tabdeal_signal.data_sources.contracts import (
+    DataProvenance as module_provenance,
+    DataQualityStatus as module_quality,
+    DataSnapshotMetadata as module_metadata,
+    NormalizedSnapshot as module_snapshot,
+    ReadOnlyDataSource as module_source,
+    SourceKind as module_kind,
+)
+
+
+def test_data_source_package_exports_match_contract_symbols() -> None:
+    assert DataProvenance is module_provenance
+    assert DataQualityStatus is module_quality
+    assert DataSnapshotMetadata is module_metadata
+    assert NormalizedSnapshot is module_snapshot
+    assert ReadOnlyDataSource is module_source
+    assert SourceKind is module_kind
+
+
+def test_data_source_package_exports_are_explicit_and_stable() -> None:
+    assert data_source_exports == [
+        "DataProvenance",
+        "DataQualityStatus",
+        "DataSnapshotMetadata",
+        "NormalizedSnapshot",
+        "ReadOnlyDataSource",
+        "SourceKind",
+    ]
